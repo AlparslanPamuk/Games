baf6cca1b9ce6f7f58970dc0ad4736d8ead1e6aa
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-05-27 23:32:13 +0300

    Fourth Commit

--- Games/Form1.Designer.cs
+++ Games/Form1.Designer.cs
@@ -28,21 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.groupBox1.ForeColor = System.Drawing.SystemColors.ActiveBorder;
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(677, 625);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
             // 
             // label1
             // 
@@ -62,29 +53,42 @@
             this.label2.TabIndex = 2;
             this.label2.Text = "label2";
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(686, 593);
+            this.panel1.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(968, 649);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Games/Form1.cs
+++ Games/Form1.cs
@@ -18,24 +18,72 @@ namespace Games
         }
 
         Snake Our_snake = new Snake();
+        Direction our_direction;
         PictureBox[] pb_snakeParts;
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            this.Text = "Mustafa Alparslan Pamuk || Snake Game";
             pb_snakeParts = new PictureBox[0];
             for (int i = 0; i < 3; i++)
             {
+                Array.Resize(ref pb_snakeParts, pb_snakeParts.Length + 1); // burdaki her eklendiği zaman bunu bir arttır dizinini uzunluğu.
                 pb_snakeParts[i] = pb_add();
             }
         }
         private PictureBox pb_add()
         {
-            Array.Resize(ref pb_snakeParts, pb_snakeParts.Length + 1);
+
             PictureBox pb = new PictureBox();
             pb.Size = new Size(10, 10);
             pb.BackColor = Color.White;
             pb.Location = Our_snake.GetPos(pb_snakeParts.Length - 1);
+            panel1.Controls.Add(pb);
+
             return pb;
         }
+
+        private void Pb_update()
+        {
+            for (int i = 0; i < pb_snakeParts.Length; i++)
+            {
+                pb_snakeParts[i].Location = Our_snake.GetPos(i);
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
+            {
+                our_direction = new Direction(0, -10);
+
+            }
+
+            else if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
+            {
+                our_direction = new Direction(0, 10);
+
+            }
+
+            else if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+            {
+                our_direction = new Direction(-10, 0);
+
+            }
+
+            else if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+            {
+                our_direction = new Direction(10, 0);
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Our_snake.Move(our_direction);
+            Pb_update();
+        }
     }
-}
+}    
+    
+
