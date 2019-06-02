677dd8496dd4db61ca107181512b8355a3a7e078
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-06-02 23:06:32 +0300

    Car Racing Game
    First

---
+++ CarRacing/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace CarRacing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureboxx2 = new System.Windows.Forms.PictureBox();
+            this.pictureboxx3 = new System.Windows.Forms.PictureBox();
+            this.pictureboxx4 = new System.Windows.Forms.PictureBox();
+            this.pictureboxx5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureboxx2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureboxx3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureboxx4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureboxx5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureboxx2
+            // 
+            this.pictureboxx2.BackColor = System.Drawing.Color.White;
+            this.pictureboxx2.Location = new System.Drawing.Point(183, -23);
+            this.pictureboxx2.Name = "pictureboxx2";
+            this.pictureboxx2.Size = new System.Drawing.Size(10, 127);
+            this.pictureboxx2.TabIndex = 0;
+            this.pictureboxx2.TabStop = false;
+            // 
+            // pictureboxx3
+            // 
+            this.pictureboxx3.BackColor = System.Drawing.Color.White;
+            this.pictureboxx3.Location = new System.Drawing.Point(183, 120);
+            this.pictureboxx3.Name = "pictureboxx3";
+            this.pictureboxx3.Size = new System.Drawing.Size(10, 127);
+            this.pictureboxx3.TabIndex = 1;
+            this.pictureboxx3.TabStop = false;
+            // 
+            // pictureboxx4
+            // 
+            this.pictureboxx4.BackColor = System.Drawing.Color.White;
+            this.pictureboxx4.Location = new System.Drawing.Point(183, 262);
+            this.pictureboxx4.Name = "pictureboxx4";
+            this.pictureboxx4.Size = new System.Drawing.Size(10, 127);
+            this.pictureboxx4.TabIndex = 2;
+            this.pictureboxx4.TabStop = false;
+            this.pictureboxx4.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureboxx5
+            // 
+            this.pictureboxx5.BackColor = System.Drawing.Color.White;
+            this.pictureboxx5.Location = new System.Drawing.Point(183, 405);
+            this.pictureboxx5.Name = "pictureboxx5";
+            this.pictureboxx5.Size = new System.Drawing.Size(10, 127);
+            this.pictureboxx5.TabIndex = 3;
+            this.pictureboxx5.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.White;
+            this.pictureBox5.Location = new System.Drawing.Point(1, -1);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(10, 453);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.White;
+            this.pictureBox6.Location = new System.Drawing.Point(370, -1);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(10, 453);
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(382, 453);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureboxx5);
+            this.Controls.Add(this.pictureboxx4);
+            this.Controls.Add(this.pictureboxx3);
+            this.Controls.Add(this.pictureboxx2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureboxx2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureboxx3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureboxx4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureboxx5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureboxx2;
+        private System.Windows.Forms.PictureBox pictureboxx3;
+        private System.Windows.Forms.PictureBox pictureboxx4;
+        private System.Windows.Forms.PictureBox pictureboxx5;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
